7f76c9783260251587ab4110b2ee7b31b12240bc
sonsolyom <nagydsolyom@gmail.com>
2021-01-09 08:25:31 +0100

    showRepos method added
    Added functionality of showing
    latest repositories,
    made new div in index for repos,
    made showRepos and clearRepos methods,
    added new async fetch to github.js

--- app.js
+++ app.js
@@ -21,10 +21,12 @@ searchUser.addEventListener('keyup', (e) => {
         } else {
           //Show profile
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       });
   } else {
     //Clear profile
     ui.clearProfile();
+    ui.clearRepos();
   }
 });
--- github.js
+++ github.js
@@ -2,16 +2,22 @@ class Github {
   constructor(){
     this.client_id = 'f53e2bfcf52cbdae4abb';
     this.client_secret = 'f50e3612b1b0465f100cf32af4015d8917a58261';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       //profile: profile is the same
-      profile 
+      profile,
+      repos
     }
   }
 }
